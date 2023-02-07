78b8040250dca48ba19044fb867e85e8f869c8a4
TexHaPb <texhapb@bk.ru>
2023-02-07 11:48:25 +0300

    spinner

--- src/pages/CarsPage.jsx
+++ src/pages/CarsPage.jsx
@@ -1,12 +1,30 @@
+import { useEffect, useState } from 'react';
+
 import { CarsList } from '../components/CarsList';
+import { Spinner } from '../components/Spinner';
 
 import cars from '../db/cars.json';
 
 const CarsPage = () => {
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		const timeout = setTimeout(() => {
+			setLoading(false);
+		}, 1500);
+
+		return () => clearTimeout(timeout);
+	}, [setLoading]);
+
 	return (
 		<>
 			<h1>Filter</h1>
-			<CarsList type="list" cars={cars} />
+			{loading ?
+				<Spinner />
+				: <CarsList type="list" cars={cars} />
+			}
+
+
 		</>
 
 	);
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,11 +1,32 @@
+import { useEffect, useState } from 'react';
+
 import { CarsList } from '../components/CarsList';
+import { Spinner } from '../components/Spinner';
 
 import cars from '../db/cars.json';
 cars = cars.sort((a, b) => b.dateCreate - a.dateCreate).slice(0, 6);
 
 const MainPage = () => {
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		const timeout = setTimeout(() => {
+			setLoading(false);
+		}, 1500);
+
+		return () => clearTimeout(timeout);
+	}, [setLoading]);
+
 	return (
-		<CarsList type="flat" title="Последние объявления" cars={cars} />
+		<>
+			<h1>Объявления о продаже авто</h1>
+			<p>Тут нужен вдохновляющий текст про сервис</p>
+
+			{loading ?
+				<Spinner />
+				: <CarsList type="flat" title="Последние объявления" cars={cars} />
+			}
+		</>
 	);
 };
 
